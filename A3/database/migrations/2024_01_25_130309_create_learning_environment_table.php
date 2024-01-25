922ab385fcf0042070ca04ac448cf00986e0670b
Alejandroradaduque_10 <aradaduque@gmail.com>
2024-01-25 08:15:08 -0500

    Migraciones y seeders 3
    MODIFICACION LEARNIG,CAREERSEEDER

---
+++ A3/database/migrations/2024_01_25_130309_create_learning_environment_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('learning_environment', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('Nombre');
+            $table->string('capacity')->comment('Capacidad');
+            $table->string('area_mt2')->comment('Area En Metros Cuadrados');
+            $table->string('floor')->comment('Piso');
+            $table->string('inventory')->comment('inventario');
+            $table->foreignId('type_id')->constrained('environment_type')
+            ->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('location_id')->constrained('location')
+            ->onDelete('cascade')->onUpdate('cascade');
+            $table->string('status')->comment('Estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('learning_environment');
+    }
+};
